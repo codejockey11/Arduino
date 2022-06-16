26d65f372e868b1b082b6c371ac65ae7d66fca3c
codejockey11 <98759225+codejockey11@users.noreply.github.com>
2022-06-15 20:01:46 -0400

    Add files via upload

---
+++ Well/Well/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 6/14/2022 10:49:47 PM
+ * Author : Steve
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
